de375013780b23236c1118dde946200d57873659
veniovinco <73258161+veniovinco@users.noreply.github.com>
2022-07-27 02:23:53 -0400

    Create keydown.js

---
+++ JavaScript/DOM/keydown.js
@@ -0,0 +1,9 @@
+<html>
+    <body>
+        <script>
+            document.addEventListener('keydown', (event) => {
+                alert(event.key)
+            })
+        </script>
+    </body>
+</html>
